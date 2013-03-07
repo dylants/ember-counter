6ae8c4ce24f340d89bc2d3ddcf9c5bddc7dbdd1e
Dylan Smith <dylantsmith@gmail.com>
2013-03-07 00:19:08 -0600

    use local storage instead of REST

--- js/models/store.js
+++ js/models/store.js
@@ -1,3 +1,8 @@
 CounterApp.Store = DS.Store.extend({
-	revision: 12
+	revision: 12,
+	adapter: 'CounterApp.LSAdapter'
+});
+
+CounterApp.LSAdapter = DS.LSAdapter.extend({
+	namespace: 'counterapp-emberjs'
 });
